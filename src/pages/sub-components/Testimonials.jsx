8e296a3708cd713546c273811d41675d4578febb
sainath <sb9codes@gmail.com>
2025-05-17 12:51:43 +0530

    background colors change

--- src/pages/sub-components/Testimonials.jsx
+++ src/pages/sub-components/Testimonials.jsx
@@ -155,7 +155,7 @@ const Testimonials = () => {
   };
 
   return (
-    <div className="py-16 bg-gradient-to-r from-indigo-100 to-purple-100">
+    <div className="py-16 bg-gradient-to-r from-green-200 to-red-200">
       <div className="container mx-auto px-4">
         <div className="text-center mb-12">
           <h2
